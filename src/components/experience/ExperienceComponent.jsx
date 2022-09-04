e13b4b6d56cc713f51182d4a492d8f1d1f65f946
Eftekhar Alam <eftialam@hotmail.com>
2022-09-04 11:40:24 +0600

    experience data fetched

--- src/components/experience/ExperienceComponent.jsx
+++ src/components/experience/ExperienceComponent.jsx
@@ -5,7 +5,8 @@ import {BsPatchCheckFill} from 'react-icons/bs'
 const ExperienceComponent = () => {
 
   const [stacks, fetchStack] = useState([]);
-  const [expe, fetchExp] = useState([]);
+  const [frontexpe, fetchFrontExp] = useState([]);
+  const [backtexpe, fetchBackExp] = useState([]);
   
   useEffect(() => {
     const url = 'http://127.0.0.1:8000/api/experiences';
@@ -13,8 +14,10 @@ const ExperienceComponent = () => {
       try{
         const res = await fetch(url);
         const json = await res.json();
-        fetchStack(json.stack);
-        console.log(json.stack);
+        fetchStack(json.data.stack);
+        fetchFrontExp(json.data.frontend);
+        fetchBackExp(json.data.backend);
+        // console.log(json.stack);
       }catch(error){
         console.log('Error',error)
       }
@@ -32,14 +35,36 @@ const ExperienceComponent = () => {
               <div className="experience__frontend" key={index}>
               <h3>{item.experience_stack}</h3>
               <div className="experience__content">
-
-                <article className='experience__details'>
-                  <BsPatchCheckFill className='experience__details-icon' />
-                  <div>
-                  <h4>HTML</h4>
-                  <small className='text-light'>Experienced</small>
-                  </div>
-                </article>
+                {
+                  (item.experience_stack == 'Frontend Development' ? <>
+                  {
+                    frontexpe.map((fx, index2) => (
+                    <article key={index2} className='experience__details'>
+                      <BsPatchCheckFill className='experience__details-icon' />
+                      <div>
+                      <h4>{fx.experience}</h4>
+                      <small className='text-light'>{fx.experience_level}</small>
+                      </div>
+                    </article>
+                    ))
+                   
+                  }
+                  </> : <>
+                  {
+                    backtexpe.map((bx, index3) => (
+                    <article key={index3} className='experience__details'>
+                      <BsPatchCheckFill className='experience__details-icon' />
+                      <div>
+                      <h4>{bx.experience}</h4>
+                      <small className='text-light'>{bx.experience_level}</small>
+                      </div>
+                    </article>
+                    ))
+                   
+                  }
+                  </>)
+                }
+                
                 
               </div>  
             </div>
